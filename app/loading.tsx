9a04c5d870797a37f4a949194c7693a65ac823a9
Muki <muki@users.noreply.github.com>
2026-02-25 14:02:34 +0100

    ui: use splash image for Next.js loading screen

---
+++ app/loading.tsx
@@ -0,0 +1,12 @@
+export default function Loading() {
+  return (
+    <div
+      className="min-h-screen w-full"
+      style={{
+        backgroundImage: "url(/splash.png)",
+        backgroundSize: "cover",
+        backgroundPosition: "center",
+      }}
+    />
+  )
+}
